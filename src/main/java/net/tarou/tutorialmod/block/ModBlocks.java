d0c9a37347dbded29693a0236666e723d5603fd5
Taro9000 <tarou9000.77+github@gmail.com>
2025-07-24 13:36:59 -0600

    Removed unused import statements

--- src/main/java/net/tarou/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/tarou/tutorialmod/block/ModBlocks.java
@@ -7,7 +7,6 @@ import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
--- src/main/java/net/tarou/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/tarou/tutorialmod/block/custom/MagicBlock.java
@@ -16,7 +16,6 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.tarou.tutorialmod.item.ModItems;
 import net.tarou.tutorialmod.util.ModTags;
 
 import java.util.List;
